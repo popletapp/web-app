aec74cbfe359b1d8b0c3b393433a5b1939193735
Blake <bsward8@gmail.com>
2019-07-04 23:57:40 +1000

    fix(Loader.jsx): fix loader rendering improperly

--- src/components/internal/load/Loader.jsx
+++ src/components/internal/load/Loader.jsx
@@ -11,44 +11,44 @@ class Loader extends Component {
         return (
             <div className='poplet-root center-on-page'>
                 <h1 className='loading-poplet-label'>Loading Poplet...</h1>
-                <div class="preloader-wrapper big active">
-                    <div class="spinner-layer spinner-blue">
-                        <div class="circle-clipper left">
-                        <div class="circle"></div>
-                        </div><div class="gap-patch">
-                        <div class="circle"></div>
-                        </div><div class="circle-clipper right">
-                        <div class="circle"></div>
+                <div className="preloader-wrapper big active">
+                    <div className="spinner-layer spinner-blue">
+                        <div className="circle-clipper left">
+                        <div className="circle"></div>
+                        </div><div className="gap-patch">
+                        <div className="circle"></div>
+                        </div><div className="circle-clipper right">
+                        <div className="circle"></div>
                         </div>
                     </div>
 
-                    <div class="spinner-layer spinner-red">
-                        <div class="circle-clipper left">
-                        <div class="circle"></div>
-                        </div><div class="gap-patch">
-                        <div class="circle"></div>
-                        </div><div class="circle-clipper right">
-                        <div class="circle"></div>
+                    <div className="spinner-layer spinner-red">
+                        <div className="circle-clipper left">
+                        <div className="circle"></div>
+                        </div><div className="gap-patch">
+                        <div className="circle"></div>
+                        </div><div className="circle-clipper right">
+                        <div className="circle"></div>
                         </div>
                     </div>
 
-                    <div class="spinner-layer spinner-yellow">
-                        <div class="circle-clipper left">
-                        <div class="circle"></div>
-                        </div><div class="gap-patch">
-                        <div class="circle"></div>
-                        </div><div class="circle-clipper right">
-                        <div class="circle"></div>
+                    <div className="spinner-layer spinner-yellow">
+                        <div className="circle-clipper left">
+                        <div className="circle"></div>
+                        </div><div className="gap-patch">
+                        <div className="circle"></div>
+                        </div><div className="circle-clipper right">
+                        <div className="circle"></div>
                         </div>
                     </div>
 
-                    <div class="spinner-layer spinner-green">
-                        <div class="circle-clipper left">
-                        <div class="circle"></div>
-                        </div><div class="gap-patch">
-                        <div class="circle"></div>
-                        </div><div class="circle-clipper right">
-                        <div class="circle"></div>
+                    <div className="spinner-layer spinner-green">
+                        <div className="circle-clipper left">
+                        <div className="circle"></div>
+                        </div><div className="gap-patch">
+                        <div className="circle"></div>
+                        </div><div className="circle-clipper right">
+                        <div className="circle"></div>
                         </div>
                     </div>
                 </div>
