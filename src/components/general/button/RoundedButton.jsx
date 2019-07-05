056ea1ea393a2b7353486481706043cda897bfe6
Blake <bsward8@gmail.com>
2019-07-05 23:45:45 +1000

    refact(RoundedButton.jsx): refact to class and other minor changes

--- src/components/general/button/RoundedButton.jsx
+++ src/components/general/button/RoundedButton.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Button.scss';
 
-export default ({ label, color = 'red', waves = true, icon = 'add', className, small }) => {
-    return (
-        <div className='btn-floating-container'>
-            <div className='floating-btn-button'>
-                <button className={`${small ? 'btn-small' : ''} btn-floating ${waves ? 'waves-effect waves-light' : ''} ${color} ${className || ''}`}>
-                    <i className="material-icons">{icon}</i>
-                </button>
+class RoundedButton extends Component {
+    constructor ({ label, color, icon, small }) {
+        super();
+        this.label = label;
+        this.color = color || 'red';
+        this.icon = icon || 'add';
+        this.small = small || false;
+    }
+
+    render() {
+        const { small, icon, color, label } = this.props;
+        return (
+            <div className='btn-floating-container'>
+                <div className='floating-btn-button'>
+                    <button {...this.props}
+                    className={`${small ? 'btn-small' : ''} btn-floating ${color}`}>
+                        <i className="material-icons">{icon}</i>
+                    </button>
+                </div>
+                {label && <div className='center floating-btn-label'><label>{label}</label></div>}
             </div>
-            {label && <div className='center floating-btn-label'><label>{label}</label></div>}
-        </div>
-    );
+        );
+    };
 }
+
+export default RoundedButton;
