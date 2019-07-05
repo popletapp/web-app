11c4d52d9853ca444bc25dbb693965826d68710b
Blake <bsward8@gmail.com>
2019-07-05 23:47:15 +1000

    refact(reducers/board.js): reduce code on board reducer

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,14 +1,9 @@
-import Poplet from './../';
-
 const board = (state = {}, action) => {
-  console.log(state, action)
   switch (action.type) {
     case 'CREATE_BOARD':
       return state
     case 'SWITCH_BOARD':
-      console.log(action.board)
-      Poplet.boards.selected = action.board;
-      state = Poplet.boards.selected;
+      state = action.board;
       return state;
     default:
       return state;
