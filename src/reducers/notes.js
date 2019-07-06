9b9b1331e9e3bb648d2c60f99ff13effb822c6f6
Blake <bsward8@gmail.com>
2019-07-07 02:12:05 +1000

    feat(reducers/notes): async support for fetching and retrieving notes

--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -1,20 +1,49 @@
-const notes = (state = [], action) => {
+export const notes = (state = {
+  isFetching: false,
+  didInvalidate: false,
+  items: []
+}, action) => {
+  console.log(action.type)
   switch (action.type) {
     case 'BEGIN_CREATE_NOTE':
-      console.log('Begin create note')
-      return [ ...state, { title: 'Title', content: 'Content' } ];
+      return Object.assign({}, state, {
+        items: [ ...state.items, { title: 'Title', content: 'Content' } ]
+      });
     case 'CREATE_NOTE':
-      return [ ...state, action.note ];
+      return Object.assign({}, state, {
+        items: [ ...state.items, action.note ]
+      });
     case 'UPDATE_NOTE':
-      const array = state;
-      const index = array.findIndex(note => note.id === action.note.id);
-      array[index] = action.note;
-      return array;
-    case 'INITIALIZE_NOTES':
-      return action.notes;
+      const old = state;
+      const index = old.items.findIndex(note => note.id === action.note.id);
+      old.items[index] = action.note;
+      return old;
+    case 'REQUEST_NOTES':
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: true
+      })
+    case 'RECEIVE_NOTES':
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        items: action.notes,
+        lastUpdated: action.receivedAt
+      })
     default:
       return state;
   }
 }
 
-export default notes
+export const notesByBoard = (state = {}, action) => {
+  switch (action.type) {
+    case 'INVALIDATE_NOTE':
+    case 'RECEIVE_NOTES':
+    case 'REQUEST_NOTES':
+      return Object.assign({}, state, {
+        [action.board.toString()]: notes(state[action.board], action)
+      })
+    default:
+      return state
+  }
+}
