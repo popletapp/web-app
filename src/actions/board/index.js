15a16614201f45c1a34412f9753bc851aeb3ff8f
Blake <bsward8@gmail.com>
2019-07-07 02:04:35 +1000

    fix(actions/board): rename and add new actions

--- src/actions/board/index.js
+++ src/actions/board/index.js
@@ -1,11 +1,16 @@
-import { CREATE_BOARD, SWITCH_BOARD } from '../../constants/ActionTypes';
+import { CREATE_BOARD, SELECT_BOARD, POPULATE_BOARDS } from '../../constants/ActionTypes';
 
 export const createBoard = (board) => ({
   type: CREATE_BOARD,
   board
 });
 
-export const switchBoard = (board) => ({
-  type: SWITCH_BOARD,
-  board
+export const selectBoard = (boardId) => ({
+  type: SELECT_BOARD,
+  board: boardId
+});
+
+export const populateBoards = (array) => ({
+  type: POPULATE_BOARDS,
+  boards: array
 });
