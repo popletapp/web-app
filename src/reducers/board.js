36785b112033d68a2ae541fab84afa728ced32c5
Blake <bsward8@gmail.com>
2019-07-07 02:12:27 +1000

    feat(reducers/board): better support for populating boards via reducer

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,13 +1,30 @@
-const board = (state = {}, action) => {
+export const selectedBoard = (state = {}, action) => {
   switch (action.type) {
     case 'CREATE_BOARD':
       return state
-    case 'SWITCH_BOARD':
-      state = action.board;
-      return state;
+    case 'SELECT_BOARD':
+      console.log(action)
+      return action.board; // Board ID
     default:
       return state;
   }
 }
 
-export default board
+export const boards = (state = {}, action) => {
+  switch (action.type) {
+    case 'CREATE_BOARD':
+      return state
+    case 'RECEIVE_BOARDS':
+    case 'POPULATE_BOARDS':
+    case 'REQUEST_BOARDS':
+      return action.array.reduce((obj, item) => {
+        obj[item.id.toString()] = item
+        return obj
+      }, {});
+    case 'CACHE_BOARD':
+    case 'UPDATE_BOARD':
+      return { ...state, [action.board.id]: action.board };
+    default:
+      return state;
+  }
+}
