a580e9b460443a87618969c5a07375d092acc487
Blake <bsward8@gmail.com>
2019-07-07 02:11:28 +1000

    feat: export new modules

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,1 +1,15 @@
+// Board
+export { default as getBoard } from './boards/getBoard.js';
+export { default as switchBoard } from './boards/switchBoard.js';
+
+// Notes
+export { default as createNote } from './notes/createNote.js';
+export { default as deleteNote } from './notes/deleteNote.js';
+export { default as receiveNote } from './notes/receiveNote.js';
+export { default as updateNote } from './notes/updateNote.js';
+
+// Users
+export { default as getCurrentUser } from './user/getCurrentUser.js';
+export { default as getUserBoards } from './user/getUserBoards.js';
+
 export { default as Modal } from './Modal';
