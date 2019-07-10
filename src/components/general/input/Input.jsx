a1e29bf57a6198faf903a3bf08a7a6c567a308cc
Blake <bsward8@gmail.com>
2019-07-11 00:34:23 +1000

    feat(Input.jsx): general improvements w/ props

--- src/components/general/input/Input.jsx
+++ src/components/general/input/Input.jsx
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 import './Input.scss';
 
 class Input extends Component {
-    constructor ({ icon = 'search', prefixIcon = false, placeholder }) {
+    constructor ({ icon, prefixIcon = false, placeholder }) {
       super();
       this.icon = icon;
       this.prefixIcon = prefixIcon;
-      this.placeholder = placeholder;
+      this.placeholder = placeholder || '';
     }
 
     render () {
-        const { icon, prefixIcon, placeholder } = this;
+        const { icon, prefixIcon } = this.props;
+        const dontInclude = [ 'prefixIcon' ]
         return <div className='text-input-container'>
             {icon && prefixIcon && 
               <div className='input-icon'>
                 <i className='material-icons'>{icon}</i>
               </div>
             }
-            <div className='text-input'
-             placeholder={placeholder || ''} 
+            <div 
+            {...Object.fromEntries(Object.entries(this.props).filter(item => !dontInclude.includes(item[0])))}
+             className='text-input'
              contentEditable='true' 
              role='textbox' 
              spellCheck='false' 
