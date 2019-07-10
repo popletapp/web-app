68470cfb7c0f0df9349063475f1c3f8e91abbd59
Blake <bsward8@gmail.com>
2019-07-11 00:29:33 +1000

    feat: error message constants

---
+++ src/constants/ErrorMessages.js
@@ -0,0 +1,3 @@
+export const BOARD_CREATE_NAME_REQUIRED = 'You need to enter a name for this board!';
+export const BOARD_CREATE_NAME_TOO_SHORT = 'The name of this board needs to be longer than 2 characters';
+export const BOARD_CREATE_NAME_TOO_LONG = 'The name of this board needs to be shorter than 64 characters';
