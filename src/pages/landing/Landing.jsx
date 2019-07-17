69876d4bcbf22092467767da9d5c46799c9ea928
Blake <bsward8@gmail.com>
2019-07-18 02:05:59 +1000

    feat: update landing page styling and make nicer

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -9,13 +9,10 @@ class Landing extends Component {
           <div className='inner'>
             <div className='title'>
               <img alt='Poplet' src='./assets/icons/poplet_white.svg' width='64' height='64'></img>
-            </div>
-            <h1 className='main-title'>Poplet</h1>
+              <h1 className='main-title'>Poplet</h1>
+            </div>      
             <div className='lower-half animated animatedFadeInUp fadeInUp'>
-              <h3 className='bio'>I'm a web and software developer based in Australia.</h3>
-
-              <div className='scrolldown-icon avatar-small'></div>
-              <p className='scrolldown'>Scroll down to see things I've worked on!</p>
+              <h3 className='bio'></h3>
             </div>
           </div>
         </section>
