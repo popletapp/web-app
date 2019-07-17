d86be4b99f6473dedfb3efbf9bfec108f2e61323
Blake <bsward8@gmail.com>
2019-07-17 20:00:17 +1000

    feat(index.js): attempt to move code away from main file loaded

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store.js';
 
 import './index.scss';
-import Application from './App';
+import './App.scss';
+import Application from './components/internal/main/App';
 import CriticalFailure from './components/internal/load/CriticalFailure';
 import Login from './components/login/Login';
 import Loader from './components/internal/load/Loader';
@@ -28,73 +30,29 @@ const Poplet = {
 console.log(Poplet)
 export default Poplet;
 
-async function render () {
-    await axios.get('/gateway/connect')
-        .then(() => {
-            ReactDOM.render(<Loader />, document.getElementById('root'));
-        })
-        .catch(() => {
-            ReactDOM.render(<CriticalFailure />, document.getElementById('root'));
-        });
+async function renderStatic () {
+    ReactDOM.render(
+        <BrowserRouter>
+            <Application />
+        </BrowserRouter>
+    , document.getElementById('root'));
+}
 
-    store.subscribe(() => console.log('Store: ', store.getState()));
-
-    const ws = new WebSocket(Poplet.ws.BASE_URL);
-    ws.onmessage = (e) => {
-        const data = JSON.parse(e.data);
-        console.log(data);
-        if (data.type) {
-            store.dispatch(data);
-        } else {
-            console.log(`Received websocket but no type information was provided: ${data}`)
-        }
-    };
-    Poplet.ws = { ...Poplet.ws, ...ws };
-
-    const getCurrentUser = () => axios.get(`/user/me`).then(res => res.data);
-    const token = localStorage.getItem('token');
-    if (!token) {
-        return ReactDOM.render(<Login />, document.getElementById('root'));
-    } else {
-        axios.defaults.headers.common['Authorization'] = token;
-        const user = await getCurrentUser();
-        console.log(`Logged in as ${user.username} (${user.id})`)
-        Poplet.user = user;
-    }
-
-    const getUserBoards = () => axios.get(`/user/${Poplet.user.id}/boards`).then(res => res.data);
-    const getBoard = (boardID) => axios.get(`/board/${boardID}`).then(res => res.data);
-    
-    const boards = await getUserBoards();
-
-    Poplet.boards = []; // Array of board objects
-    // Get the current list of ID's of the boards that this user is in and create new board objects from the ID's
-    // Fetch from the API to convert ID's to objects
-    Poplet.boards = await Promise.all(boards.map(board => getBoard(board)))
-    store.dispatch({ type: 'POPULATE_BOARDS', array: Poplet.boards })
-
-    Poplet.users = [];
-    Poplet.notes = [];
-    // Same applies for users: fetch all users inside of the board that were fetched above (each board will have a `members` property with IDs)
-    for (const board of Poplet.boards) {
-        if (board) {
-            Poplet.users = Poplet.users.concat(board.members); // Array of cached users
-            Poplet.notes = Poplet.notes.concat(board.notes); // Some notes should be cached to prevent having to call the API each time
-        }  
-    }
-
+async function renderApplication () {
     ReactDOM.render(
         <Provider store={store}>
-            <Application board={Poplet.boards.selected} />
+            <BrowserRouter>
+                <Application board={Poplet.boards[0]} />
+            </BrowserRouter>
         </Provider>
     , document.getElementById('root'));
 }
 
-render();
+renderStatic();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-export { render };
+export { renderApplication, renderStatic };
