506a304d24c5a22ae58d843ea5876128e13ffb94
Blake <bsward8@gmail.com>
2019-07-18 01:57:57 +1000

    feat: more additions to landing page (WIP)

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -5,7 +5,20 @@ class Landing extends Component {
   render () {
     return (
       <div className='landing'>
-        <img alt='Poplet' src='./assets/icons/poplet_white.svg' width='64' height='64'></img>
+        <section className='top introduction row'>
+          <div className='inner'>
+            <div className='title'>
+              <img alt='Poplet' src='./assets/icons/poplet_white.svg' width='64' height='64'></img>
+            </div>
+            <h1 className='main-title'>Poplet</h1>
+            <div className='lower-half animated animatedFadeInUp fadeInUp'>
+              <h3 className='bio'>I'm a web and software developer based in Australia.</h3>
+
+              <div className='scrolldown-icon avatar-small'></div>
+              <p className='scrolldown'>Scroll down to see things I've worked on!</p>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
