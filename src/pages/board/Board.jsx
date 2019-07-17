d8129a1995de759df76b793b2c178767b986bef3
Blake <bsward8@gmail.com>
2019-07-17 19:52:03 +1000

    feat: move board component to pages

---
+++ src/pages/board/Board.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Board, Chatroom } from '../../components';
+import Poplet from './../../'
+import { connect } from 'react-redux';
+import { selectBoard } from './../../actions/board';
+import { getNotes } from './../../actions/note';
+import { switchBoard } from './../../modules';
+
+function mapStateToProps (state) {
+    return {
+        board: state.boards[state.selectedBoard],
+        notes: state.notes
+    }
+}
+
+function mapDispatchToProps (dispatch) {
+    return {
+        selectBoard: boardId => dispatch(selectBoard(boardId)),
+        getNotes: boardId => dispatch(getNotes(boardId))
+    };
+}
+
+class App extends Component {
+    constructor ({ board }) {
+        super();
+        this.board = board;
+        this.state = {
+            loaded: false
+        };
+    }
+
+    async componentDidMount () {
+        const boardID = this.props.match.params.id;
+        if (boardID) {
+            await switchBoard(boardID);
+        } else {
+            await switchBoard(Poplet.boards[0].id);
+        }
+        this.setState({ loaded: true });
+    }
+
+    render () {
+        const board = this.props.board;
+        if (!this.state.loaded) {
+            return null;
+        }
+
+        setTimeout(() => {
+            window.M.AutoInit();
+        }, 20)
+        return (
+            <div className='poplet-root'>
+                <div className='modal-container'></div>
+                <Board key={board.id} object={board} notes={[]} />
+                <Chatroom chatroom={this.state.chatroom}/>
+            </div>
+        )
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
