af16c465cce23f6fc32d6224eec7fe71797feb54
Blake <bsward8@gmail.com>
2019-07-17 19:58:36 +1000

    refact(App.jsx): use react-router to route to other areas of application

--- src/components/internal/main/App.jsx
+++ src/components/internal/main/App.jsx
@@ -1,62 +1,13 @@
-import React, { Component } from 'react';
-import { Board, Chatroom } from '../..';
-import Poplet from './../../../'
-import { connect } from "react-redux";
-import { selectBoard } from './../../../actions/board';
-import { getNotes } from './../../../actions/note';
-import { switchBoard } from './../../../modules';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { Home, Board, Landing } from './../../../pages'
 
-function mapStateToProps (state) {
-    return {
-        board: state.boards[state.selectedBoard],
-        notes: state.notes
-    }
+export default function App () {
+  return (
+    <Switch>
+      <Route exact path='/' component={Landing} />
+      <Route exact path='/home' component={Home} />
+      <Route path='/boards/:id' component={Board} />
+    </Switch>
+  )
 }
-
-function mapDispatchToProps (dispatch) {
-    return {
-        selectBoard: boardId => dispatch(selectBoard(boardId)),
-        getNotes: boardId => dispatch(getNotes(boardId))
-    };
-}
-
-class App extends Component {
-    constructor ({ board }) {
-        super();
-        this.board = board;
-        this.state = {
-            loaded: false
-        };
-    }
-
-    async changeBoard (id) {
-        await switchBoard(id);
-        this.setState({
-            loaded: true
-        });
-    }
-
-    async componentDidMount () {
-        await this.changeBoard(Poplet.boards[0].id);
-    }
-
-    render () {
-        const board = this.props.board || {};
-        if (!this.state.loaded) {
-            return null;
-        }
-
-        setTimeout(() => {
-            window.M.AutoInit();
-        }, 20)
-        return (
-            <div className='poplet-root'>
-                <div className='modal-container'></div>
-                <Board key={board.id} object={board} notes={[]} />
-                <Chatroom chatroom={this.state.chatroom}/>
-            </div>
-        )
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
