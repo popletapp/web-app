411a489c3e8ec03db0484cca6e124a1d4a76428b
Blake <bsward8@gmail.com>
2019-07-17 19:49:49 +1000

    feat(Note.jsx): detailed view, better click detection on selection

--- src/components/board/note/Note.jsx
+++ src/components/board/note/Note.jsx
@@ -4,35 +4,44 @@ import hljs from 'highlight.js';
 import 'highlight.js/styles/tomorrow-night.css';
 
 import { connect } from 'react-redux';
-import { Button } from './../../';
+import { Button, NoteDetailedView } from './../../';
 import './Note.scss';
-import { updateNote, createNote } from './../../../modules';
+import { endCreateNote } from './../../../actions/note';
+import { Modal, updateNote, createNote } from './../../../modules';
+
+
+function mapStateToProps (state, props) {
+    return {
+        note: (state.notesByBoard[props.boardId] || {}).items ? state.notesByBoard[props.boardId].items.find(note => note.id === props.note.id) : props.note
+    }
+}
 
 function mapDispatchToProps (dispatch) {
     return {
-        updateNote: note => dispatch(updateNote(note)),
-        createNote: note => dispatch(createNote(note))
-    };
+        endCreateNote: () => dispatch(endCreateNote())
+    }
 }
 
 class Note extends Component {
-    constructor ({ note, boardId, exists }) {
+    constructor ({ note, boardId }) {
         super();
         this.note = note;
         this.boardId = boardId;
+
         // Whether or not this note is currently being created
-        this.exists = exists || true;
         this.state = {
             selected: false,
-            editing: false
+            editing: false,
+            exists: this.note ? !!this.note.id : false
         };
         this.listener = (event) => {
             let el = event.target;
+            if (el.matches('.btn')) return note;
             do {
                 if (el.matches('.note')) return el;
                 el = el.parentElement || el.parentNode;
             } while (el !== null && el.nodeType === 1);
-
+            
             if (el && el.classList && el.classList.contains('note')) {
                 this.onClick();
             } else {
@@ -49,13 +58,16 @@ class Note extends Component {
     }
 
     onFocus () {
-        this.setState({
-            editing: true
-        })
+        this.setState({ editing: true });
     }
 
-    onClick (event) {
-        this.setState({ selected: true })
+    view (event) {
+        event.preventDefault();
+        new Modal(<NoteDetailedView note={this.props.note} boardId={this.boardId} />).create()
+    }
+
+    onClick () {
+        this.setState({ selected: true });
     }
 
     unselect () {
@@ -63,27 +75,34 @@ class Note extends Component {
     }
 
     async onBlur (event, type) {
-        const { note, exists } = this.props;
+        const { note } = this.props;
+        const { exists } = this.state;
 
         const content = event.target.textContent.replace('<br>', '\\n');
         const newNote = { ...note, [type]: content };
 
         this.setState({
-            note: newNote,
-            editing: false
+            editing: false,
+            saving: true
         })
         if (note[type] !== newNote[type]) {
             if (exists) {
-                updateNote(this.boardId, newNote);
+                await updateNote(this.boardId, newNote);
             } else {
-                createNote(this.boardId, newNote);
+                this.setState({ 
+                    unmounted: true, 
+                    exists: true 
+                });
+                await createNote(this.boardId, newNote);
+                this.props.endCreateNote();
             }
         }
+        this.setState({ saving: false });
     }
     
     componentDidMount () {
         const { note } = this.props;
-        if (note.options && note.options.position) {
+        if (note && note.options && note.options.position) {
             this.setState({
                 style: {
                     top: note.options ? note.options.position.y : 0,
@@ -103,22 +122,26 @@ class Note extends Component {
     }
 
     render () {
-        const { note } = this.props;
-        const { editing, selected } = this.state;
+        const { note, listView } = this.props;
+        const { editing, selected, style } = this.state;
+        if (!note || this.state.unmounted) {
+            return null;
+        }
         if (!note.options) {
             note.options = {};
         }
 
         return (
-            <div draggable={true}
+            <div draggable={selected}
                 onClick={(event) => this.onClick(event)}
                 onDragStart={(event) => this.onDragStart(event)}
-                className={`draggable note ${note.options.color || 'blue-grey'} darken-1${selected ? ' selected' : ''}`} 
-                style={{ ...(this.state.style || {}), width: 'fit-content' }}>
+                className={`note ${!note.options.color ? 'blue-grey ' : ''}darken-1${selected ? ' selected draggable' : ''}`} 
+                style={{ ...(!listView ? style : {}), width: 'fit-content', backgroundColor: note.options.color || ''}}>
                 {selected && <div className='selected-checkmark'><i className='material-icons'>checkmark</i></div>}
                 <div className='note-content white-text'>
                     <div contentEditable={selected ? 'true' : 'false'}
                     suppressContentEditableWarning={true} 
+                    onFocus={(e) => this.onFocus(e, 'title')} 
                     onBlur={(e) => this.onBlur(e, 'title')} 
                     className='note-header'>
                         {note.title > 64 ? `${note.title.slice(0, 61)}...` : note.title}
@@ -132,13 +155,19 @@ class Note extends Component {
                     </div>
                 </div>
                 <div className='note-footer'>
-                    <Button text='View' color='red'></Button>
-                    <h6 className='note-footer-label'>Last modified on <strong>{new Date(note.modifiedAt).toDateString()}</strong></h6>
-                    <h6 className='note-footer-label'>by <strong>{note.modifiedBy && note.modifiedBy.username}</strong></h6>
+                    <Button onClick={(e) => this.view(e)} className='note-btn-view'><i className='material-icons' style={{ fontSize: '13px' }}>description</i> View</Button>
+                    <div className='vertical-rule' style={{ height: 24 }}></div>
+                    {/* TODO: Implement comments */}
+                    {true && <div className='note-footer-comments-container'>
+                        <i className='material-icons' style={{ fontSize: '14px' }}>comment</i> 0
+                    </div>}
+                    <div className='note-footer-settings-container'>
+                        <i className='material-icons' style={{ fontSize: '22px' }}>settings</i>
+                    </div>
                 </div>
             </div>
         )
     }
 }
 
-export default connect(null, mapDispatchToProps)(Note);
+export default connect(mapStateToProps, mapDispatchToProps)(Note);
