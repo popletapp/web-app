88f205cb7cbc2c514dbcccd25c3c847a1bfbe188
Blake <bsward8@gmail.com>
2019-07-17 19:47:38 +1000

    feat: UPDATE_VIEW action on board actions

--- src/actions/board/index.js
+++ src/actions/board/index.js
@@ -1,4 +1,4 @@
-import { CREATE_BOARD, SELECT_BOARD, POPULATE_BOARDS } from '../../constants/ActionTypes';
+import { CREATE_BOARD, SELECT_BOARD, POPULATE_BOARDS, UPDATE_VIEW } from '../../constants/ActionTypes';
 
 export const createBoard = (board) => ({
   type: CREATE_BOARD,
@@ -8,6 +8,12 @@ export const createBoard = (board) => ({
 export const selectBoard = (boardId) => ({
   type: SELECT_BOARD,
   board: boardId
+});
+
+export const updateView = (boardId, view) => ({
+  type: UPDATE_VIEW,
+  board: boardId,
+  view
 });
 
 export const populateBoards = (array) => ({
