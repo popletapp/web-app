b27100f6d96d38627e3bcaa40d7bfbce0f357d3c
Blake <bsward8@gmail.com>
2019-07-17 19:52:35 +1000

    feat: new landing page for popletapp.com

---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Landing.scss';
+
+class Landing extends Component {
+  render () {
+    return (
+      <div className='landing'>
+        <img alt='Poplet' src='./assets/icons/poplet_white.svg' width='64' height='64'></img>
+      </div>
+    )
+  }
+}
+
+export default Landing;
