9f2f5198ba74db7e5c30cbcca6a19b543b3f74f3
Blake <bsward8@gmail.com>
2019-07-17 19:59:55 +1000

    feat: new action types

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,6 +1,7 @@
 // Board
 export const CREATE_BOARD = 'CREATE_BOARD';
 export const SELECT_BOARD = 'SELECT_BOARD';
+export const UPDATE_VIEW = 'UPDATE_VIEW';
 export const POPULATE_BOARDS = 'POPULATE_BOARDS';
 
 // Note
@@ -10,5 +11,11 @@ export const RECEIVE_NOTES = 'RECEIVE_NOTES';
 export const INITIALIZE_NOTES = 'INITIALIZE_NOTES';
 
 export const BEGIN_CREATE_NOTE = 'BEGIN_CREATE_NOTE';
+export const END_CREATE_NOTE = 'END_CREATE_NOTE';
 export const CREATE_NOTE = 'CREATE_NOTE';
 export const UPDATE_NOTE = 'UPDATE_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE';
+
+// User
+export const INITIALIZE_USER = 'INITIALIZE_USER';
+export const UPDATE_USER = 'UPDATE_USER';
