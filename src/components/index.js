2d10f222445dedc656c37d932cefc375dd5a74ab
Blake <bsward8@gmail.com>
2019-07-25 16:10:39 +1000

    feat: export new components

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@
 export { default as Board } from './board/Board';
 export { default as Note } from './board/note/Note';
 export { default as TopBar } from './board/topbar/TopBar';
+export { default as Group } from './board/group/Group';
 
 // Chatroom
 export { default as Chatroom } from './chatroom/Chatroom';
@@ -13,11 +14,16 @@ export { default as RoundedButton } from './general/button/RoundedButton';
 export { default as Modal } from './general/modal/Modal';
 export { default as BoardCreationModal } from './general/modal/BoardCreationModal';
 export { default as NoteDetailedView } from './general/modal/NoteDetailedView';
+export { default as BoardInviteMembersModal } from './general/modal/BoardInviteMembersModal';
+export { default as BoardJoinModal } from './general/modal/BoardJoinModal';
 export { default as Input } from './general/input/Input';
+export { default as Editor } from './general/editor/Editor';
 export { default as ColorPicker } from './general/colorpicker/ColorPicker';
+export { default as CustomDragLayer } from './general/drag/CustomDragLayer';
 
 // Internal
 export { default as CriticalFailure } from './internal/load/CriticalFailure';
 export { default as Loader } from './internal/load/Loader';
+export { default as Connecting } from './internal/load/Connecting';
 export { default as App } from './internal/main/App';
 export { default as PopletBase } from './internal/main/PopletBase';
