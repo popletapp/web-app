fd5a968d0ec702b51a13828df57ab3bcf6ab7cac
Blake <bsward8@gmail.com>
2019-07-25 16:12:05 +1000

    feat: new routes, 404 page and modal support

--- src/components/internal/main/App.jsx
+++ src/components/internal/main/App.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Home, Board, Landing, Login } from './../../../pages';
+import { Home, Board, Landing, Login, SignUp } from './../../../pages';
+import 'react-router-modal/css/react-router-modal.css';
+import { ModalContainer, ModalRoute } from 'react-router-modal';
+import { BoardJoinModal } from './../../../components';
 
 class App extends Component {
   render () {
     return (
-      <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route exact path='/home' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route path='/boards/:id' component={Board} />
-      </Switch>
+      <div>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={SignUp} />
+          <ModalRoute path='/boards/join' component={BoardJoinModal} />
+          <Route path='/boards/create' component={Board} />
+          <Route path='/boards/:id' component={Board} />
+        </Switch>
+        <ModalContainer />
+      </div>
     );
   }
 }
