64b68ed59f9d91436ef2484904a15359e6f74a25
Blake <bsward8@gmail.com>
2019-07-25 16:15:03 +1000

    feat: updateNote no longer sends API request

--- src/modules/notes/updateNote.js
+++ src/modules/notes/updateNote.js
@@ -1,12 +1,5 @@
 import Poplet from '../..';
-import { updateNote } from '../../actions/note';
-import axios from 'axios';
 
 export default async (boardID, note) => {
-  const { id } = note;
-  const store = Poplet.store;
-  console.log(note.content);
-  const value = await axios.patch(`/boards/${boardID}/notes/${id}`, note).then(res => res.data);
-  await store.dispatch(updateNote(boardID, value || note));
-  return value;
+  Poplet.ws.emit('message', { type: 'UPDATE_NOTE', board: boardID, note });
 };
