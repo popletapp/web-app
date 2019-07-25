e49bb6ed2ccf8fa25d4c222a0ea466fe07776827
Blake <bsward8@gmail.com>
2019-07-25 16:13:46 +1000

    feat: export new modules

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,12 +2,25 @@
 export { default as getBoard } from './boards/getBoard.js';
 export { default as switchBoard } from './boards/switchBoard.js';
 export { default as createBoard } from './boards/createBoard.js';
+export { default as joinBoard } from './boards/joinBoard.js';
+export { default as createInvite } from './boards/createInvite.js';
+export { default as checkInvite } from './boards/checkInvite.js';
+export { default as beginSelection } from './boards/beginSelection.js';
+export { default as endSelection } from './boards/endSelection.js';
+
+// Groups
+export { default as getGroups } from './groups/getGroups.js';
+export { default as createGroup } from './groups/createGroup.js';
+export { default as updateGroup } from './groups/updateGroup.js';
+export { default as deleteGroup } from './groups/deleteGroup.js';
+export { default as addNoteToGroup } from './groups/addNoteToGroup.js';
 
 // Notes
 export { default as createNote } from './notes/createNote.js';
 export { default as deleteNote } from './notes/deleteNote.js';
 export { default as receiveNote } from './notes/receiveNote.js';
 export { default as updateNote } from './notes/updateNote.js';
+export { default as saveNote } from './notes/saveNote.js';
 
 // Users
 export { default as getCurrentUser } from './user/getCurrentUser.js';
