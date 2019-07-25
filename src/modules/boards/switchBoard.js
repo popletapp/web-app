d286ca196d2499f7fd30536ed369f007c606ecac
Blake <bsward8@gmail.com>
2019-07-25 16:13:34 +1000

    feat(switchBoard.js): load groups when switching boards

--- src/modules/boards/switchBoard.js
+++ src/modules/boards/switchBoard.js
@@ -1,4 +1,5 @@
 import Poplet from '../..';
+import getGroups from './../groups/getGroups';
 import { selectBoard } from '../../actions/board';
 import { getNotes, receiveNotes } from '../../actions/note';
 import getBoard from './getBoard.js';
@@ -8,6 +9,7 @@ export default async (id) => {
   const board = await getBoard(id);
   if (board) {
     store.dispatch(selectBoard(id));
+    const groups = await getGroups(id);
     let notes = await store.dispatch(getNotes(id));
     if (notes.notes) {
       notes = notes.notes;
@@ -15,7 +17,7 @@ export default async (id) => {
       await store.dispatch(receiveNotes(id, notes));
     }
     console.log(`Switching to ${board.id} (${board.name})`, board, notes);
-    return { board, notes };
+    return { board, notes, groups };
   } else {
     return Promise.reject(new Error('Board ID is invalid'));
   }
