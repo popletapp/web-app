4ebeda34c3bfebf61ea985c580fdbda4f08958e9
Blake <bsward8@gmail.com>
2019-07-25 16:14:41 +1000

    feat: clean-up code and don't load placeholder

--- src/modules/internal/established.js
+++ src/modules/internal/established.js
@@ -1,8 +1,14 @@
 import store from './../../store.js';
 import axios from 'axios';
 import Poplet from './../../';
+import openSocket from 'socket.io-client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Connecting } from '../../components';
 
-export default async (id) => {
+let LAST_HEARTBEAT_TIMESTAMP = Date.now();
+
+export default async () => {
   const getCurrentUser = () => axios.get(`/users/me`).then(res => res.data);
   const token = localStorage.getItem('token');
   if (!token) {
@@ -14,18 +20,31 @@ export default async (id) => {
     store.dispatch({ type: 'INITIALIZE_USER', user });
     Poplet.user = user;
 
-    const ws = new WebSocket(Poplet.ws.BASE_URL);
-    ws.onmessage = (e) => {
-      const data = JSON.parse(e.data);
-      console.log(data);
-      if (data.type) {
-        store.dispatch(data);
-      } else {
-        console.log(`Received websocket but no type information was provided: ${data}`);
-      }
-    };
-    Poplet.ws = { ...Poplet.ws, ...ws };
+    const socket = openSocket('http://localhost:7777');
+    console.log(socket);
+    Poplet.ws = socket;
   }
+
+  Poplet.ws.emit('authorize', { userID: Poplet.user.id, authToken: token });
+  Poplet.ws.on('message', (data) => {
+    console.log(data);
+    if (data.type) {
+      store.dispatch(data);
+    } else {
+      console.log(`Received websocket but no type information was provided: ${data}`);
+    }
+  });
+
+  Poplet.ws.emit('heartbeat');
+  setInterval(() => {
+    Poplet.ws.emit('heartbeat');
+    if (Date.now() - LAST_HEARTBEAT_TIMESTAMP > 30e3) {
+      ReactDOM.render(<Connecting />, document.querySelector('#root'));
+    }
+  }, 10000);
+  Poplet.ws.on('heartbeat', () => {
+    LAST_HEARTBEAT_TIMESTAMP = Date.now();
+  });
 
   const getUserBoards = () => axios.get(`/users/${Poplet.user.id}/boards`).then(res => res.data);
 
@@ -35,7 +54,7 @@ export default async (id) => {
   // Get the current list of ID's of the boards that this user is in and create new board objects from the ID's
   // Fetch from the API to convert ID's to objects
   Poplet.boards = boards;
-  Poplet.boards = !Poplet.boards.length ? [{ id: '1', name: 'Not a board (default placeholder)', avatar: null, chatrooms: [{}] }] : Poplet.boards;
+  Poplet.boards = !Poplet.boards.length ? [] : Poplet.boards;
   store.dispatch({ type: 'POPULATE_BOARDS', array: Poplet.boards });
 
   Poplet.users = [];
