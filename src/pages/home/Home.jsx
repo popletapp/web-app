dda45891aefb042da8ebf2ed3c73e5134832855a
Blake <bsward8@gmail.com>
2019-07-25 16:16:23 +1000

    feat(Home.jsx): begin work on activity feed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { PopletBase, Avatar } from './../../components';
+import { PopletBase, Avatar, Note } from './../../components';
 import { Link } from 'react-router-dom';
 import './Home.scss';
 
 function mapStateToProps (state) {
   return {
     boards: state.boards,
+    notes: state.notes,
     user: state.user
   };
 }
 
 class Home extends PopletBase {
+  async componentDidMount () {
+    await this.init();
+  }
+
   render () {
-    const { boards, user } = this.props;
+    const { boards, user, notes: { items } } = this.props;
     return (
       <div className='poplet-root center-on-page home'>
         <div className='home-header'>
           <img src='./assets/icons/poplet_white_no_bg.svg' width='32' height='32' alt=''></img>
           <h1 className='home-header-text'>
-                        Home
+            Home
           </h1>
 
           <div className='user-container'>
@@ -41,9 +46,23 @@ class Home extends PopletBase {
                 </Link>
               );
             })}
+            {/* TODO: link to /boards/join */}
+            <Link className='board-item' style={{ backgroundColor: '#65a376' }} to='/boards/join'>
+              <div className='board-item-name'>
+                Join a Board
+              </div>
+            </Link>
           </div>
-          <div className='home-content'>
-
+          <div className='activity-feed'>
+            <div className='activity-feed-title'>Activity Feed</div>
+            <div className='activity-feed-subtitle'>Recently Modified</div>
+            {(() => {
+              if (!items.length) {
+                return <div className='activity-feed-content'>Nothing new on your feed.</div>;
+              } else {
+                return Object.values(items).filter((a, b) => b.modifiedAt - a.modifiedAt).map(note => <Note key={note.id} id={note.id} preview boardId={(boards.find(board => board.notes.includes(note.id)) || {}).id} />);
+              }
+            })()}
           </div>
         </div>
       </div>
