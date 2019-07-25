8a167bed90ef227d0a64dcf33819b18462de1b62
Blake <bsward8@gmail.com>
2019-07-25 16:07:42 +1000

    feat: add group support for actions

--- src/actions/board/index.js
+++ src/actions/board/index.js
@@ -1,4 +1,16 @@
-import { CREATE_BOARD, SELECT_BOARD, POPULATE_BOARDS, UPDATE_VIEW } from '../../constants/ActionTypes';
+import {
+  CREATE_BOARD,
+  SELECT_BOARD,
+  JOIN_BOARD,
+  POPULATE_BOARDS,
+  UPDATE_VIEW,
+  BEGIN_SELECTION,
+  END_SELECTION,
+  RECEIVE_GROUPS,
+  CREATE_GROUP,
+  UPDATE_GROUP,
+  DELETE_GROUP
+} from '../../constants/ActionTypes';
 
 export const createBoard = (board) => ({
   type: CREATE_BOARD,
@@ -7,6 +19,11 @@ export const createBoard = (board) => ({
 
 export const selectBoard = (boardId) => ({
   type: SELECT_BOARD,
+  board: boardId
+});
+
+export const joinBoard = (boardId) => ({
+  type: JOIN_BOARD,
   board: boardId
 });
 
@@ -19,4 +36,37 @@ export const updateView = (boardId, view) => ({
 export const populateBoards = (array) => ({
   type: POPULATE_BOARDS,
   boards: array
+});
+
+export const receiveGroups = (groups) => ({
+  type: RECEIVE_GROUPS,
+  groups
+});
+
+export const createGroup = (boardId, group) => ({
+  type: CREATE_GROUP,
+  board: boardId,
+  group
+});
+
+export const updateGroup = (boardId, group) => ({
+  type: UPDATE_GROUP,
+  board: boardId,
+  group
+});
+
+export const deleteGroup = (boardId, group) => ({
+  type: DELETE_GROUP,
+  board: boardId,
+  group
+});
+
+export const beginSelection = (obj) => ({
+  type: BEGIN_SELECTION,
+  selection: obj // object containing x1, x2, y1, y2
+});
+
+export const endSelection = (obj) => ({
+  type: END_SELECTION,
+  selection: obj
 });
