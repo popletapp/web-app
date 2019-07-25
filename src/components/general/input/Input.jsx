175f75414d08cc0398138378eae057a657c4cfdd
Blake <bsward8@gmail.com>
2019-07-25 16:10:25 +1000

    feat: changes to input component

--- src/components/general/input/Input.jsx
+++ src/components/general/input/Input.jsx
@@ -7,12 +7,13 @@ class Input extends Component {
     this.icon = icon;
     this.prefixIcon = prefixIcon;
     this.placeholder = placeholder || '';
+    this.state = {};
   }
 
   render () {
-    const { icon, prefixIcon } = this.props;
+    const { icon, prefixIcon, className } = this.props;
     const dontInclude = ['prefixIcon'];
-    return <div className='text-input-container'>
+    return <div className={`${className} text-input-container`}>
       {icon && prefixIcon &&
               <div className='input-icon'>
                 <i className='material-icons'>{icon}</i>
@@ -22,16 +23,15 @@ class Input extends Component {
         {...Object.fromEntries(Object.entries(this.props).filter(item => !dontInclude.includes(item[0])))}
         className='text-input'
         contentEditable='true'
+        suppressContentEditableWarning={true}
         role='textbox'
         spellCheck='false'
-        style={{ outline: 'none', userSelect: 'text', whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>
+        style={{ outline: 'none', userSelect: 'text', whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>{this.props.children || ''}
       </div>
       {icon && !prefixIcon &&
-              <div className='input-icon'>
-                <i className='material-icons'>{icon}</i>
-              </div>
-      }
-
+        <div className='input-icon'>
+          <i className='material-icons'>{icon}</i>
+        </div>}
     </div>;
   }
 }
