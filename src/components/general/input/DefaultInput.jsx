336fa60b9a13acd5e8342f1cb73d3a1cddb96008
Blake <bsward8@gmail.com>
2019-07-29 03:04:25 +1000

    feat: input that uses <input>

---
+++ src/components/general/input/DefaultInput.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Input.scss';
+
+class DefaultInput extends Component {
+  constructor ({ name, type, placeholder, value }) {
+    super();
+    this.name = name;
+    this.type = type;
+    this.placeholder = placeholder || '';
+    this.value = value;
+    this.state = {};
+  }
+
+  render () {
+    const { name, type, placeholder, value, children } = this.props;
+    return (
+      <input className='text-input'
+        name={name || 'input'}
+        spellCheck='false'
+        type={type || 'text'}
+        placeholder={placeholder || ''}
+        maxLength='999'
+        defaultValue={value || ''}>
+        {children || null}
+      </input>
+    );
+  }
+}
+
+export default DefaultInput;
