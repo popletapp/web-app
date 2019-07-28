909a2b81676512ca5e2bbd3464e6f182b92ab8a9
Blake <bsward8@gmail.com>
2019-07-29 03:11:00 +1000

    feat: loader changes

--- src/components/internal/load/Loader.jsx
+++ src/components/internal/load/Loader.jsx
@@ -12,12 +12,12 @@ class Loader extends Component {
       <div className='loading fadeOutZoomIn'>
         <h1 className='poplet-title' style={{ position: 'absolute', left: '45%', top: '35%', margin: '0' }}>Poplet</h1>
         <div className='container' style={{ display: 'flex', flexDirection: 'column' }}>
-          <div className='poplet-icon-bg-top' style={{ height: '47.6%', width: '100%' }} />
+          <div className='poplet-icon-bg-top' style={{ height: '47.2%', width: '100%' }} />
         </div>
         <div className='container' style={{ display: 'flex', flexDirection: 'row' }}>
-          <div className='poplet-icon-bg-left' style={{ height: '100%', width: '48%' }} />
-          <div className='poplet-icon-bg-bottom' style={{ position: 'absolute', bottom: '0', left: '47%', height: '48%', width: '120px' }} />
-          <div className='poplet-icon-bg-right' style={{ position: 'absolute', left: '52%', height: '100%', width: '47%' }} />
+          <div className='poplet-icon-bg-left' style={{ height: '100%', width: '48.2%' }} />
+          <div className='poplet-icon-bg-bottom' style={{ position: 'absolute', bottom: '0', left: '47%', height: '47.2%', width: '120px' }} />
+          <div className='poplet-icon-bg-right' style={{ position: 'absolute', left: '52%', height: '100%', width: '48%' }} />
         </div>
         <div className='poplet-loading'>
           <div className='poplet-loading-icon'>
