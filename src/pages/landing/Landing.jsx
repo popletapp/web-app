2b4b6c4b82c06d8f846c77983d62d937c261b9e0
Blake <bsward8@gmail.com>
2019-07-29 03:10:07 +1000

    feat: better landing page

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import { NavBar } from './../../components';
 import './Landing.scss';
 
 class Landing extends Component {
   render () {
     return (
-      <div className='landing'>
-        <section className='top introduction row'>
-          <div className='inner'>
-            <div className='title'>
-              <img alt='Poplet' src='./assets/icons/poplet_white.svg' width='64' height='64'></img>
-              <h1 className='main-title'>Poplet</h1>
-            </div>      
-            <div className='lower-half animated animatedFadeInUp fadeInUp'>
-              <h3 className='bio'></h3>
+      <div>
+        <NavBar />
+        <div className='landing'>
+          <section className='landing-cinematic'>
+            <div className='inner'>
+              <div className='title'>
+                <img alt='Poplet' src='./assets/icons/poplet_white.svg' width='64' height='64'></img>
+                <h1 className='main-title'>Poplet</h1>
+              </div>
+              <div className='lower-half animated animatedFadeInUp fadeInUp'>
+                <h3 className='bio'></h3>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
     );
   }
