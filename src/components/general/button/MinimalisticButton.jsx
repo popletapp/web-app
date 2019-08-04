e09de8d3eb5fb4db4beed78a786d3b35ed532a7b
Blake <bsward8@gmail.com>
2019-08-05 00:08:41 +1000

    feat: changes to some generic components

--- src/components/general/button/MinimalisticButton.jsx
+++ src/components/general/button/MinimalisticButton.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import { Flex } from './../../';
 import { joinClasses } from './../../../util';
 import './Button.scss';
 
 class MinimalisticButton extends Component {
   render () {
-    const { icon, className, style, onClick } = this.props;
+    const { icon, className, style, onClick, children, size } = this.props;
     return (
       <button
         style={style}
         onClick={onClick}
         className={joinClasses('minimalistic-button', className)}>
-        <i className="material-icons">{icon}</i>
+        <Flex direction='row' align='center'>
+          <i className={joinClasses('material-icons', children ? 'minimalistic-button-icon' : null)} style={{ fontSize: size || '1.3rem' }}>{icon}</i>
+          {children}
+        </Flex>
       </button>
     );
   }
