d17453622149229cd9b647bd15c70431721276d3
Blake <bsward8@gmail.com>
2020-01-16 05:41:18 +1100

    fix(established.js): better heartbeat implementation

--- src/modules/internal/established.js
+++ src/modules/internal/established.js
@@ -1,6 +1,6 @@
 import store from './../../store.js';
 import axios from 'axios';
-import Poplet from './../../';
+import Poplet, { render } from './../../';
 import openSocket from 'socket.io-client';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -8,6 +8,19 @@ import { Connecting } from '../../components';
 import { getBoards } from './../../actions/board';
 
 let LAST_HEARTBEAT_TIMESTAMP = Date.now();
+const MAX_KEEP_ALIVE_TIME = 50e3;
+const HEARTBEAT_INTERVAL = 10e3;
+const RETRY_TIMES = [5e2, 1e3, 2e3, 5e3, 10e3, 30e3, 60e3, 120e3];
+
+const connectSocket = () => {
+  let socket = null;
+  try {
+    socket = openSocket(process.env.NODE_ENV === 'development' ? 'ws://localhost:7777' : 'wss://popletapp.com', { transports: ['websocket'] });
+  } catch (e) {
+    Poplet.log.prefix(Poplet.log.PREFIX_TYPES.GATEWAY).error(`Error whilst trying to open websocket conncection: ${e.message || e}`);
+  }
+  Poplet.ws = socket;
+};
 
 export default async () => {
   const getCurrentUser = () => axios.get(`/users/me`).then(res => res.data);
@@ -16,20 +29,11 @@ export default async () => {
     document.location.replace('/login');
   } else {
     axios.defaults.headers.common['Authorization'] = token;
-
     const user = await getCurrentUser();
     Poplet.log.prefix(Poplet.log.PREFIX_TYPES.GATEWAY).info(`Logged in as ${user.username} (${user.id})`);
     store.dispatch({ type: 'INITIALIZE_USER', user });
     Poplet.user = user;
-
-    let socket = null;
-    try {
-      socket = openSocket(process.env.NODE_ENV === 'development' ? 'ws://localhost:7777' : 'wss://popletapp.com', { transports: ['websocket'] });
-    } catch (e) {
-      console.log(e);
-    }
-
-    Poplet.ws = socket;
+    connectSocket();
   }
 
   Poplet.ws.emit('authorize', { userID: Poplet.user.id, authToken: token });
@@ -37,34 +41,60 @@ export default async () => {
     if (data.type) {
       store.dispatch(data);
     } else {
-      console.log(`Received websocket but no type information was provided: ${data}`);
+      Poplet.log.prefix(Poplet.log.PREFIX_TYPES.GATEWAY).warn(`Received websocket but no type information was provided: ${data}`);
     }
   });
 
-  let reconnecting = false;
+  let retries = 0;
   Poplet.ws.emit('heartbeat');
-  setInterval(() => {
-    Poplet.ws.emit('heartbeat');
-    if (Date.now() - LAST_HEARTBEAT_TIMESTAMP > 50e3) {
-      ReactDOM.render(<Connecting />, document.querySelector('#root'));
-      reconnecting = true;
-    } else {
-      if (reconnecting) {
-        window.location.reload();
+  let disconnected = false;
+  let retryTime = RETRY_TIMES[0];
+  const reconnect = () => {
+    retryTime = Math.min(RETRY_TIMES[retries] || Infinity, 1e4 * 80);
+    setTimeout(() => {
+      // If the last heartbeat hasn't been returned in more than 50 seconds, render the reconnect view
+      if (Date.now() - LAST_HEARTBEAT_TIMESTAMP > MAX_KEEP_ALIVE_TIME) {
+        retries++;
+        Poplet.log.prefix(Poplet.log.PREFIX_TYPES.GATEWAY).warn(`Heartbeat was not received from server. Retrying in ${retryTime}ms (${retries} attempts)`);
+        ReactDOM.render(<Connecting />, document.querySelector('#root'));
+        disconnected = true;
+        reconnect();
+      } else {
+        if (disconnected) {
+          disconnected = false;
+          Poplet.log.prefix(Poplet.log.PREFIX_TYPES.GATEWAY).info(`Reconnected to gateway after ${retries} retries`);
+          connectSocket();
+          render();
+        }
       }
+    }, retryTime); // max reconnect time of 120 seconds
+  };
+
+  const POLLING_TIME = 500;
+  let amountPolled = 0;
+  setInterval(() => {
+    amountPolled++;
+    if (amountPolled * POLLING_TIME > HEARTBEAT_INTERVAL) {
+      Poplet.ws.emit('heartbeat');
+      Poplet.log.prefix(Poplet.log.PREFIX_TYPES.GATEWAY).debug('Heartbeat sent');
+      amountPolled = 0;
     }
-  }, 10000);
+    if (Date.now() - LAST_HEARTBEAT_TIMESTAMP > MAX_KEEP_ALIVE_TIME) {
+      reconnect();
+    }
+  }, POLLING_TIME);
+
   Poplet.ws.on('heartbeat', () => {
     LAST_HEARTBEAT_TIMESTAMP = Date.now();
+    Poplet.log.prefix(Poplet.log.PREFIX_TYPES.GATEWAY).debug('Heartbeat received');
   });
 
   const boards = await store.dispatch(getBoards(Poplet.user.id));
-  Poplet.boards = []; // Array of board objects
+  Poplet.boards = !boards.length ? [] : boards; // Array of board objects
   // Get the current list of ID's of the boards that this user is in and create new board objects from the ID's
   // Fetch from the API to convert ID's to objects
-  Poplet.boards = boards;
-  Poplet.boards = !Poplet.boards.length ? [] : Poplet.boards;
 
+  // Initialize variables
   Poplet.users = [];
   Poplet.notes = [];
   // Same applies for users: fetch all users inside of the board that were fetched above (each board will have a `members` property with IDs)
@@ -74,5 +104,7 @@ export default async () => {
       Poplet.notes = Poplet.notes.concat(board.notes); // Some notes should be cached to prevent having to call the API each time
     }
   }
+
+  // Send CONNECT dispatch
   store.dispatch({ type: 'CONNECT' });
 };
