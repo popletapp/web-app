ede3f6ab85c5c57a6e8f475e58f4f3d7a3553f88
Blake <bsward8@gmail.com>
2020-01-18 11:20:50 +1100

    feat: animation for opening/closing chatroom

--- src/modules/chatroom/toggleChatroomVisibility.js
+++ src/modules/chatroom/toggleChatroomVisibility.js
@@ -2,7 +2,15 @@ export default () => {
   const container = document.querySelector('.chatroom-container');
   if (container.style.display) {
     container.style.display = null;
+    container.classList.add('chatroom-entering');
+    setTimeout(() => {
+      container.classList.remove('chatroom-entering');
+    }, 550);
   } else {
-    container.style.display = 'none';
+    container.classList.add('chatroom-exiting');
+    setTimeout(() => {
+      container.classList.remove('chatroom-exiting');
+      container.style.display = 'none';
+    }, 550);
   }
 };
