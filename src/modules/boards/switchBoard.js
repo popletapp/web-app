d9accd3c01f2a6f0b9b3d3f65e13552be6470734
Blake <bsward8@gmail.com>
2020-01-18 11:15:32 +1100

    fix: minor chatroom improvements

--- src/modules/boards/switchBoard.js
+++ src/modules/boards/switchBoard.js
@@ -1,6 +1,6 @@
 import Poplet from '../..';
 import { selectBoard, getMembers, getGroups } from '../../actions/board';
-import { getChatrooms } from '../../actions/chatroom';
+import { getChatrooms, getChatroomComments } from '../../actions/chatroom';
 import { getNotes } from '../../actions/note';
 import getBoard from './getBoard.js';
 
@@ -13,8 +13,9 @@ export default async (id) => {
     const notes = await store.dispatch(getNotes(id));
     const members = await store.dispatch(getMembers(id)).then(g => g.members);
     const chatrooms = await store.dispatch(getChatrooms(id)).then(g => g.chatrooms);
-    console.log(`Switching to ${board.id} (${board.name})`, { board, notes, groups, members, chatrooms });
-    return { board, notes, groups, members, chatrooms };
+    const selectedChatroomComments = chatrooms[0] ? await store.dispatch(getChatroomComments(chatrooms[0].id)).then(g => g.comments) : null;
+    console.log(`Switching to ${board.id} (${board.name})`, { board, notes, groups, members, chatrooms, selectedChatroomComments });
+    return { board, notes, groups, members, chatrooms, selectedChatroomComments };
   } else {
     return Promise.reject(new Error('Board ID is invalid'));
   }
