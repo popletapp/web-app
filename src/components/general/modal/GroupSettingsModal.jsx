c2d203744473021d7ad68828d569a21ba411b8d8
Blake <bsward8@gmail.com>
2020-01-18 11:19:27 +1100

    feat: new modal for group settings

--- src/components/general/modal/GroupSettingsModal.jsx
+++ src/components/general/modal/GroupSettingsModal.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import Modal from './Modal';
-import { DefaultInput, Flex, Button } from '../..';
+import { DefaultInput, Flex, Button, ColorPicker, FlexChild, Input } from '../..';
+import { connect } from 'react-redux';
 import Poplet from '../../..';
 import { switchBoard, joinBoard, checkInvite } from '../../../modules';
 import './Modal.scss';
 import * as errors from '../../../constants/ErrorMessages.js';
 
+function mapStateToProps (state, props) {
+  return {
+    group: state.groupsByBoard[props.boardID][props.groupID]
+  }
+}
+
 class GroupSettingsModal extends Modal {
-  constructor ({ location }) {
+  constructor ({ boardID, groupID, location }) {
     super();
     this.state = {
       name: '',
+      type: 0,
       error: null
     };
+    this.boardID = boardID;
+    this.groupID = groupID;
     this.location = location;
   }
 
-  async check ({ code }) {
-    const invite = await checkInvite(code);
-    if (!invite) {
-      throw new Error(errors.BOARD_JOIN_INVITE_LINK_INVALID);
-    } else if (code.length < 2) {
-      throw new Error(errors.BOARD_JOIN_INVITE_LINK_UNAUTHORIZED);
+  check ({ name }) {
+    if (!name) {
+      throw new Error(errors.BOARD_CREATE_NAME_REQUIRED);
+    } else if (name.length < 2) {
+      throw new Error(errors.BOARD_CREATE_NAME_TOO_SHORT);
+    } else if (name.length > 64) {
+      throw new Error(errors.BOARD_CREATE_NAME_TOO_LONG);
     } else {
       return null;
     }
@@ -40,15 +51,15 @@ class GroupSettingsModal extends Modal {
     }
   }
 
-  cancel () {
-
-  }
-
-  onChange (event) {
+  handleColorChange () {
 
   }
 
   render () {
+    const { group } = this.props;
+    const { error, type } = this.state;
+    console.log(group);
+    if (!group) return null;
     return (
       <div>
         <div className='modal-content'>
@@ -56,15 +67,27 @@ class GroupSettingsModal extends Modal {
             Group Settings
           </div>
           <div className='modal-body'>
-            <div className='modal-note-settings-header'>Group Name</div>
-            <DefaultInput onChange={(e) => this.onChange(e)} placeholder='Code' />
-            <br />
-            <div className='modal-note-settings-header'>Group Color</div>
-            <DefaultInput onChange={(e) => this.onChange(e)} placeholder='Code' />
+            <p className='modal-error'>{error}</p>
+
+            <Flex direction='row'>
+              <FlexChild>
+                <div className='modal-note-settings-header'>Group Name</div>
+                <Input onChange={(e) => this.onChange(e)}>{group.name}</Input>
+              </FlexChild>    
+              <br />
+              <FlexChild>
+                <div className='modal-note-settings-header'>Group Color</div>
+                <ColorPicker
+                  color={group.options ? group.options.color : '#546e7a'}
+                  onChangeComplete={(color) => this.handleColorChange(color)}
+                />
+              </FlexChild>
+            </Flex>
+
             <br />
             <div className='modal-note-settings-header'>Group Type</div>
             <Flex direction='row' className='board-creation-type'>
-              <Flex className='board-creation-type-option' align='center' direction='column'>
+              <Flex onClick={() => this.setState({ type: 0 })} className={`board-creation-type-option${type === 0 ? ' board-creation-type-option-active' : ''}`} align='center' direction='column'>
                 <Flex className='board-creation-type-option-display' align='center'>
                   <h2 className='board-creation-type-option-header'>Freeplace</h2>
                   <img src='./../../../assets/icons/freeplace.svg' width='128' height='128'></img>
@@ -72,7 +95,7 @@ class GroupSettingsModal extends Modal {
                 <div className='board-creation-type-option-desc'>Freeplace allows you to place notes without restrictions in any position</div>
               </Flex>
 
-              <Flex className='board-creation-type-option' align='center' direction='column'>
+              <Flex onClick={() => this.setState({ type: 1 })} className={`board-creation-type-option${type === 1 ? ' board-creation-type-option-active' : ''}`}  align='center' direction='column'>
                 <Flex className='board-creation-type-option-display' align='center'>
                   <h2 className='board-creation-type-option-header'>Grid</h2>
                   <img src='./../../../assets/icons/snaptogrid.svg' width='128' height='128'></img>
@@ -91,4 +114,4 @@ class GroupSettingsModal extends Modal {
   }
 }
 
-export default GroupSettingsModal;
+export default connect(mapStateToProps, null)(GroupSettingsModal);
