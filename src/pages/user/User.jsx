bfcb92ac75f54d9eba75cc75e4225ec04dac70f4
Blake <bsward8@gmail.com>
2020-01-18 11:16:52 +1100

    feat: improvements to user profiles

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { PopletBase, NavBar, Avatar, Flex, FlexChild } from '../../components';
+import { PopletBase, NavBar, Avatar, Flex, FlexChild, MinimalisticButton } from '../../components';
 import { getUser } from '../../modules';
 import './User.scss';
 
 function mapStateToProps (state, props) {
   return {
-    user: state.users.items.find(user => user.id === props.match ? props.match.params.id : state.user.id)
+    user: state.users.items.find(user => user.id === props.match ? props.match.params.id : state.user.id),
+    currentUser: state.user
   };
 }
 
@@ -20,29 +21,45 @@ class UserComponent extends PopletBase {
   }
 
   render () {
-    const { user } = this.props;
+    const { user = null, currentUser = {} } = this.props;
+    if (currentUser === null || user === null) return null;
+    const isSelf = user.id === currentUser.id;
     return (
       <div className='poplet-root center-on-page user-profile'>
         <NavBar />
-
         {(() => {
           if (user) {
             return (
               <Flex className='user-profile-container' align='left' direction='row'>
-                <FlexChild grow={0} direction='row' align='center'>
-                  <Avatar url={user.avatar} alt={user.username} size='large' />
-                  <FlexChild grow={0} align='center' className='user-profile-username' style={{ marginLeft: '24px' }}>
-                    {user.username}
+                <section>
+                  <FlexChild grow={0} shrink={0} direction='row' align='center'>
+                    <Avatar className='user-profile-avatar' url={user.avatar} alt={user.username} size='large' />
+                    <Flex grow={1} direction='row' align='center' className='user-profile-username' style={{ marginLeft: '24px' }}>
+                      <FlexChild grow={0} shrink={1} direction='row' align='center'>
+                        {user.username}
+                      </FlexChild>
+
+                      <FlexChild className='user-profile-send-fr-btn-container' grow={0} shrink={1} direction='row' align='center'>
+                        {!isSelf && <div className='user-profile-send-fr-btn btn'>
+                          Send Friend Request
+                        </div>}
+                        <MinimalisticButton icon='more_vert' color='red' className='user-profile-more-actions-btn' />
+                      </FlexChild>
+                    </Flex>
                   </FlexChild>
-                </FlexChild>
+
+                  <FlexChild className='user-profile-information' grow={0} direction='row' align='center'>
+                    <p>created {new Date(user.createdAt).toLocaleDateString()}</p>
+                  </FlexChild>
+                </section>
               </Flex>
             );
           } else {
-            return (
-              <div>
-                That actually doesn't appear to be a user...
-              </div>
-            );
+            if (user === null) {
+              return null;
+            } else {
+              return <div>That user does not exist</div>
+            }
           }
         })()}
 
