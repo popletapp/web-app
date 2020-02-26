ffe6b95668fcee5686541b214143b831b9d78b26
Blake <bsward8@gmail.com>
2020-02-26 22:03:23 +1100

    feat: find available position when creating notes/groups

--- src/modules/groups/createGroup.js
+++ src/modules/groups/createGroup.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import Poplet from '../..';
+import { findNextAvailablePosition } from '../';
 import { createGroup } from './../../actions/board';
 
 export default async (boardID, obj) => {
   const store = Poplet.store;
   const value = await axios.put(`/boards/${boardID}/groups`, { ...obj, boardID }).then(res => res.data);
+  findNextAvailablePosition(boardID, value);
   store.dispatch(createGroup(boardID, value));
   Poplet.ws.emit('message', createGroup(boardID, value));
   return value;
--- src/modules/notes/createNote.js
+++ src/modules/notes/createNote.js
@@ -1,10 +1,13 @@
 import Poplet from '../..';
 import { createNote } from '../../actions/note';
+import { findNextAvailablePosition } from '../';
 import axios from 'axios';
 
 export default async (boardID, note) => {
   const store = Poplet.store;
+  
   const value = await axios.put(`/boards/${boardID}/notes`, { ...note, boardID }).then(res => res.data);
+  findNextAvailablePosition(boardID, value);
   store.dispatch(createNote(boardID, value));
   Poplet.ws.emit('message', createNote(boardID, value));
   return value;
