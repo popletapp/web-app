1fa4258da546d29e8690de8f3626e4b8e68474df
Blake <bsward8@gmail.com>
2020-02-26 21:56:38 +1100

    feat(Input): style changes + placeholder

--- src/components/general/input/Input.jsx
+++ src/components/general/input/Input.jsx
@@ -4,7 +4,7 @@ import './Input.scss';
 
 class Input extends Component {
   render () {
-    const { icon, prefixIcon, className, onClick, style, onInput } = this.props;
+    const { icon, prefixIcon, className, onClick, style, onInput, placeholder } = this.props;
     return <div className={`${className} text-input`}>
       {icon && prefixIcon &&
               <div className='input-icon'>
@@ -19,6 +19,7 @@ class Input extends Component {
         suppressContentEditableWarning={true}
         role='textbox'
         spellCheck='false'
+        placeholder={placeholder}
         style={{ outline: 'none', userSelect: 'text', whiteSpace: 'pre-wrap', overflowWrap: 'break-word', ...style }}>{this.props.children || ''}
       </div>
       {icon && !prefixIcon &&
