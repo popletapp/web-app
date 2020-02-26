dba19016aebfadeeb3bdc960fdb31ab2871db537
Blake <bsward8@gmail.com>
2020-02-26 21:59:31 +1100

    fix: tooltips not rendering correctly

--- src/components/general/tooltip/Tooltips.jsx
+++ src/components/general/tooltip/Tooltips.jsx
@@ -10,10 +10,11 @@ function mapStateToProps (state) {
 class Tooltips extends Component {
   render () {
     const { tooltips } = this.props;
-    console.log(tooltips)
     return (
       <div className='tooltips animated'>
-        {tooltips.map((tooltip, i) => <div style={{ top: tooltip.position.y, left: tooltip.position.x }} className='tooltip' key={i}>{tooltip.content}</div>)}
+        {tooltips.map((tooltip, i) => 
+          <div style={{ top: tooltip.position.y, left: tooltip.position.x }} className='tooltip' key={i}>{tooltip.content}</div>)
+        }
       </div>
     );
   }
