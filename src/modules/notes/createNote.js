d046e7f3add659daf0adaaab266dc7c2a3b8362c
Blake <bsward8@gmail.com>
2020-02-28 06:45:22 +1100

    feat(createNote): scroll to note + animate on creation

--- src/modules/notes/createNote.js
+++ src/modules/notes/createNote.js
@@ -2,13 +2,23 @@ import Poplet from '../..';
 import { createNote } from '../../actions/note';
 import { findNextAvailablePosition } from '../';
 import axios from 'axios';
+import { permissions } from './../../util';
 
 export default async (boardID, note) => {
   const store = Poplet.store;
-  
+  if (!permissions.has('MANAGE_NOTES')) return false;
   const value = await axios.put(`/boards/${boardID}/notes`, { ...note, boardID }).then(res => res.data);
-  findNextAvailablePosition(boardID, value);
+  const { position, id } = findNextAvailablePosition(boardID, value, true);
   store.dispatch(createNote(boardID, value));
   Poplet.ws.emit('message', createNote(boardID, value));
+
+  const noteDOM = document.querySelector(`.note[data-id="${id}"]`);
+  if (noteDOM) {
+    noteDOM.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+    noteDOM.classList.add('note-creation-flash')
+    setTimeout(() => {
+      noteDOM.classList.remove('note-creation-flash')
+    }, 3e3);
+  }
   return value;
 };
