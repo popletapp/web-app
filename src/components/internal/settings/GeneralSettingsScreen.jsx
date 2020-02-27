34ac2727de10a6019652cb24e870abd585688493
Blake <bsward8@gmail.com>
2020-02-28 06:42:58 +1100

    feat(GeneralSettingsScreen): functionality w/ api

--- src/components/internal/settings/GeneralSettingsScreen.jsx
+++ src/components/internal/settings/GeneralSettingsScreen.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { DefaultInput, ToggleSwitch, Flex } from '../..';
 import { connect } from 'react-redux';
-import { updateBoard } from '../../../actions/board';
+import { updateBoard } from '../../../modules';
 
 function mapStateToProps (state, props) {
   return {
@@ -12,14 +12,26 @@ function mapStateToProps (state, props) {
 class GeneralSettingsScreen extends Component {
   async updateBoardName (e) {
     const { board } = this.props;
-    const name = e.target.textContent;
+    const name = e.target.value;
     board.name = name;
     await updateBoard(board.id, board);
-    this.setState({ editingName: false, board });
+  }
+
+  async updateBoardType (type) {
+    const { board } = this.props;
+    board.type = type;
+    await updateBoard(board.id, board);
+  }
+
+  async updateBoardResize (resize) {
+    const { board } = this.props;
+    board.autoResize = resize;
+    await updateBoard(board.id, board);
   }
 
   render () {
     const { board } = this.props;
+    const { type } = board;
     return (
       <Flex direction='column' align='left' className='board-settings-content-container'>
         <div className='board-settings-header'>
@@ -29,18 +41,35 @@ class GeneralSettingsScreen extends Component {
           Adjust general settings within this board
         </div>
 
-        <div className='board-settings-subheader'>
-          Name
-        </div>
+        <div className='board-settings-subheader'>Name</div>
         <DefaultInput
           onBlur={(e) => this.updateBoardName(e)}
           value={board.name}
           style={{ height: '32px' }}>
         </DefaultInput>
 
+        <div className='board-settings-subheader'>Board Type</div>
+        <Flex direction='row' grow={0} className='board-creation-type'>
+          <Flex onClick={() => this.updateBoardType(0)} className={`board-creation-type-option${type === 0 ? ' board-creation-type-option-active' : ''}`} align='center' direction='column'>
+            <Flex className='board-creation-type-option-display' align='center'>
+              <h2 className='board-creation-type-option-header'>Freeplace</h2>
+              <img alt='Freeplace' src='./../../../assets/icons/freeplace.svg' width='128' height='128'></img>
+            </Flex>
+            <div className='board-creation-type-option-desc'>Freeplace allows you to place notes without restrictions in any position</div>
+          </Flex>
+
+          <Flex onClick={() => this.updateBoardType(1)} className={`board-creation-type-option${type === 1 ? ' board-creation-type-option-active' : ''}`}  align='center' direction='column'>
+            <Flex className='board-creation-type-option-display' align='center'>
+              <h2 className='board-creation-type-option-header'>Grid</h2>
+              <img alt='Grid' src='./../../../assets/icons/snaptogrid.svg' width='128' height='128'></img>
+            </Flex>
+            <div className='board-creation-type-option-desc'>Snap to Grid means all notes/groups can only be placed on grid lines</div>
+          </Flex>
+        </Flex>
+
         <Flex direction='row' grow={0} className='board-settings-subheader'>
           Auto-Resize
-          <ToggleSwitch small={true} style={{ marginLeft: '24px' }} />
+          <ToggleSwitch onChange={(resize) => this.updateBoardResize(resize)} small={true} style={{ marginLeft: '24px' }} />
         </Flex>
       </Flex>
     );
