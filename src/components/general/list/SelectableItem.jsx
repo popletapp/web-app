6ed1af039625b6fdd5630179bb70b0d9ac7a0f18
Blake <bsward8@gmail.com>
2020-03-06 09:12:08 +1100

    feat(SelectableItem): add right click functionality

--- src/components/general/list/SelectableItem.jsx
+++ src/components/general/list/SelectableItem.jsx
@@ -3,10 +3,22 @@ import { joinClasses } from './../../../util';
 import './SelectableItem.scss';
 
 class SelectableItem extends Component {
+  onClick (event) {
+    const { onClick, onRightClick } = this.props;
+    if (typeof onClick === 'function') {
+      if (event.nativeEvent.which === 3) {
+        event.preventDefault();
+        onRightClick(event);
+      } else if (event.nativeEvent.which === 1) {
+        onClick(event);
+      }
+    }
+  }
+
   render () {
     const { selected, className, children } = this.props;
     return (
-      <div className='selectable-item' onClick={this.props.onClick}>
+      <div className='selectable-item' onClick={e => this.onClick(e)} onContextMenu={e => this.onClick(e)}>
         <div className={joinClasses('selectable-item-content', className, selected ? 'selectable-item-selected' : null)}>
           {children}
         </div>
