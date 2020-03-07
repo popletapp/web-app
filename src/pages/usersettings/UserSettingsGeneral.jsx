c57b662278e2894e13ac8e2f3aa3f804bb822a30
Blake <bsward8@gmail.com>
2020-03-08 09:03:07 +1100

    feat: avatar changing

--- src/pages/usersettings/UserSettingsGeneral.jsx
+++ src/pages/usersettings/UserSettingsGeneral.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Flex, FlexChild, ToggleSwitch } from '../../components';
-import { Messages } from '../../i18n';
+import { Flex, FlexChild, ToggleSwitch, Avatar, UsernameText } from '../../components';
+import { addAvatar, updateUser } from '../../modules';
+import { withTranslation } from 'react-i18next';
 
-function mapStateToProps (state, props) {
+function mapStateToProps (state) {
   return {
     user: state.user
   };
@@ -14,27 +15,58 @@ class UserSettingsGeneral extends Component {
     super();
   }
 
+  onAvatarClick () {
+    const upload = document.querySelector('.file-upload');
+    upload.click();
+  }
+
+  onAvatarSelectedFile (e) {
+    const { user } = this.props;
+    const file = e.target.files.length ? e.target.files[0] : null;
+    if (file) {
+      console.log(file)
+      addAvatar(user.id, file);
+    }
+  }
+
+  async toggleCompactMode (val) {
+    const { user } = this.props;
+    user.compact = val;
+    await updateUser(user);
+  }
+
   render () {
+    const { t, user } = this.props;
+    console.log(user)
     return (
       <FlexChild direction='column' className='user-settings-content'>
-        <h1>{Messages.SETTINGS}</h1>
+        <h1>{t("SETTINGS")}</h1>
         <hr></hr>
+
+        <form id="file" encType="multipart/form-data"><input onChange={(e) => this.onAvatarSelectedFile(e)} type='file' className='file-upload'></input></form>
+        <Flex direction='row' grow={0} className='user-settings-user'>
+          <div onClick={() => this.onAvatarClick()} className='user-settings-user-avatar-container'>
+            <Avatar id={user.id} className='user-settings-user-avatar' url={user.avatar} size='large' alt={user.username}></Avatar>
+          </div>
+          
+          <UsernameText className='user-settings-user-username'>{user.username}</UsernameText>
+        </Flex>
 
         <Flex direction='row' grow={0} className='user-settings-setting'>
           <FlexChild direction='column'>
             <header className='user-settings-header'>
-              {Messages.USER_SETTINGS_GENERAL_COMPACT_MODE}
+              {t("USER_SETTINGS_GENERAL_COMPACT_MODE")}
             </header>
             <h5 className='user-settings-setting-desc'>
-              {Messages.USER_SETTINGS_GENERAL_COMPACT_MODE_DESCRIPTION}
+              {t("USER_SETTINGS_GENERAL_COMPACT_MODE_DESCRIPTION")}
             </h5>
           </FlexChild>
-          
-          <ToggleSwitch small={true} />
+
+          <ToggleSwitch onChange={(val) => this.toggleCompactMode(val)} small={true} initialState={user.compact} />
         </Flex>
       </FlexChild>
     )
   }
 }
 
-export default connect(mapStateToProps, null)(UserSettingsGeneral);
+export default withTranslation()(connect(mapStateToProps, null)(UserSettingsGeneral));
