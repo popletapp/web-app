17fe23874a65901a6324a0e2eb6198479c35a4e8
Nevvulo <bsward8@gmail.com>
2020-06-12 11:36:18 +1000

    feat(User): implement friend feature

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Poplet from './../../index';
 import { connect } from 'react-redux';
 import { PopletBase, NavBar, Avatar, Flex, FlexChild, MinimalisticButton } from '../../components';
-import { getUser } from '../../modules';
+import { getUser, sendFriendRequest } from '../../modules';
 import './User.scss';
 import { withTranslation } from 'react-i18next';
 
@@ -21,6 +22,11 @@ class UserComponent extends PopletBase {
     }
   }
 
+  async createFriendRequest () {
+    Poplet.log.prefix(Poplet.log.PREFIX_TYPES.NETWORK).info(`Sending friend request to ${this.props.user.id}`)
+    await sendFriendRequest(this.props.user.id);
+  }
+
   render () {
     const { user = null, currentUser = {}, t } = this.props;
     if (currentUser === null || user === null) return null;
@@ -31,27 +37,31 @@ class UserComponent extends PopletBase {
         {(() => {
           if (user) {
             return (
-              <Flex className='user-profile-container' align='left' direction='row'>
+              <Flex className='user-profile-container' justify='center' direction='row'>
                 <section>
-                  <FlexChild grow={0} shrink={0} direction='row' align='center'>
-                    <Avatar className='user-profile-avatar' id={user.id} url={user.avatar} alt={user.username} size='large' />
-                    <Flex grow={1} direction='row' align='center' className='user-profile-username' style={{ marginLeft: '24px' }}>
-                      <FlexChild grow={0} shrink={1} direction='row' align='center'>
+                  <FlexChild className='user-profile-intro-content' grow={0} shrink={0} direction='column' justify='center' align='center'>
+                    
+                    <Flex grow={1} direction='row' align='center' className='user-profile-username'>
+                      <Avatar className='user-profile-avatar' id={user.id} url={user.avatar} alt={user.username} size='large' />
+                      <FlexChild className='user-profile-username-text' grow={0} shrink={1} direction='row' align='center'>
                         {user.username}
                       </FlexChild>
-
-                      <FlexChild className='user-profile-send-fr-btn-container' grow={0} shrink={1} direction='row' align='center'>
-                        {!isSelf && <div className='user-profile-send-fr-btn btn'>
-                          {t("SEND_FREIND_REQUEST")}
-                        </div>}
-                        <MinimalisticButton icon='more_vert' color='red' className='user-profile-more-actions-btn' />
-                      </FlexChild>
                     </Flex>
+
+                    <FlexChild className='user-profile-send-fr-btn-container' grow={0} shrink={1} direction='row' align='center'>
+                      {!isSelf && <Flex onClick={() => this.createFriendRequest()} className='user-profile-send-fr-btn btn' align='center' direction='row'>
+                        <i className='material-icons'>person_add</i>
+                        <span>{t("SEND_FRIEND_REQUEST")}</span>
+                      </Flex>}
+                      <MinimalisticButton icon='more_vert' color='red' className='user-profile-more-actions-btn' />
+                    </FlexChild>
+
+                    <FlexChild className='user-profile-information' grow={0} direction='row' justify='center' align='center'>
+                      <p>{t("USER_CREATED")} {new Date(user.createdAt).toLocaleDateString()}</p>
+                    </FlexChild>
                   </FlexChild>
 
-                  <FlexChild className='user-profile-information' grow={0} direction='row' align='center'>
-                    <p>{t("USER_CREATED")} {new Date(user.createdAt).toLocaleDateString()}</p>
-                  </FlexChild>
+                  
                 </section>
               </Flex>
             );
